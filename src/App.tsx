612ba8d237642690a1d29a08dd9ee2b0c67331c9
effierichman <55716328+effierichman@users.noreply.github.com>
2022-03-30 15:22:16 -0400

    finish front-end

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Column } from "./components/Column";
 import { AddNewItem } from "./components/AddNewItem";
 import { useAppState } from "./state/AppStateContext";
 import { addList } from "./state/actions";
+import { CustomDragLayer } from "./components/CustomDragLayer";
 
 export const App = () => {
   const { lists, dispatch } = useAppState();
 
   return (
     <AppContainer>
+      <CustomDragLayer />
       {lists.map((list) => (
         <Column text={list.text} key={list.id} id={list.id} />
       ))}
